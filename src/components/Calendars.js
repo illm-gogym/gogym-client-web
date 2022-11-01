2fb8ce840c7146bdfa981d53d76b88b36b024d3a
이선아 <iseon-a@iseon-aui-MacBookPro.local>
2022-11-01 21:51:25 +0900

    모달 생성 및 커스텀

--- src/components/Calendars.js
+++ src/components/Calendars.js
@@ -4,16 +4,20 @@ import React from "react";
 import $ from 'jquery';
 
 import Modal from './Modal';
+import {Link} from "react-router-dom";
+import classNames from "classnames";
 
 function Calenders() {
 	const [date, setDate] = useState(new Date());
 	const taskList = [
-		{'date': '2022-11-01 09:20', 'member': '한예슬'},
-		{'date': '2022-11-01 12:20', 'member': '김태희'},
-		{'date': '2022-11-16 17:20', 'member': '비'},
-		{'date': '2022-11-25 06:20', 'member': '한가인'},
-		{'date': '2022-11-27 20:20', 'member': '전지현'},
+		{'date': '2022. 11. 01', 'time': '09:20', 'member': '한예슬'},
+		{'date': '2022. 11. 01', 'time': '12:20', 'member': '김태희'},
+		{'date': '2022. 11. 16', 'time': '17:20', 'member': '비'},
+		{'date': '2022. 11. 25', 'time': '06:20', 'member': '한가인'},
+		{'date': '2022. 11. 27', 'time': '20:20', 'member': '전지현'},
 	];
+
+	const [dayList, setDayList] = useState([]);
 
 	const [modalOpen, setModalOpen] = useState(false);
 	const openModal = () => {
@@ -24,19 +28,36 @@ function Calenders() {
 	};
 
 	const onClickDay = (value, event) => {
-		// console.log($(event.target).text());
-		console.log(changeDateFormat(value));
+		var clickDay = dateSplitDay(value);
+
+		setDayList(() => {return []});
+
+		taskList.map((value, index) => {
+			var day = dateSplitDay(new Date(value.date));
+			if(day === clickDay) {
+				setDayList((prevState) => {
+					return [{'member': value.member, 'time': value.time}, ...prevState];
+				})
+			}
+		});
+
+		openModal();
 	}
 
-	const changeDateFormat = (value) => {
+	const dateSplitDay = (value) => {
+		return value.toLocaleDateString('ko-KR');
+	}
+
+	const dateSplitTime = (value) => {
 		return value.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 	}
 
 	const checkTask = (activeStartDate, date, view) => {
 		var taskArr = taskList.map((value, index) => {
-			var currentDate = new Date(value.date);
+			var currentDate = new Date(`${value.date} ${value.time}`);
+			var options = { hour: "numeric", minute: "numeric", hour12: false };
 			if(date.getMonth() === currentDate.getMonth() && date.getDate() === currentDate.getDate() ) {
-				return { member:value.member, time: currentDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} ;
+				return { member:value.member, time: currentDate.toLocaleTimeString("en-US", options)} ;
 			}
 		});
 
@@ -76,21 +97,16 @@ function Calenders() {
 
 			<Modal open={modalOpen} close={closeModal} header="오늘의 일정">
 				<ul className={'day_list'}>
-					<li className={'item'}>
-						<span className={'text'}>14:00 강민호 님</span>
-						<button type={'button'} className={'btn_manage'}>회원관리</button>
-					</li>
-					<li className={'item'}>
-						<span className={'text'}>18:30 정솔 님</span>
-						<button type={'button'} className={'btn_manage'}>회원관리</button>
-					</li>
-					<li className={'item'}>
-						<span className={'text'}>14:00 강민호 님</span>
-						<button type={'button'} className={'btn_manage'}>회원관리</button>
-					</li>
+					{dayList.map((value, index) =>
+						<li className={'item'}>
+							<span className={'text'}>
+								<strong>{value.time}</strong> {value.member} 님
+							</span>
+							<button type={'button'} className={'btn_manage'}>회원관리</button>
+						</li>
+					)}
 				</ul>
 			</Modal>
-			<button onClick={openModal}>모달팝업</button>
 		</>
 	);
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,16 +5,41 @@ import classNames from 'classnames';
 import Aside from '../components/Aside';
 
 import {Icon} from "../asset/js/icon";
+import Modal from "../components/Modal";
 
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			modalOpen: false,
+		}
 	}
 
 	componentDidMount() {
 	}
 
+	openModal = () => {
+		this.setState({
+			modalOpen: true
+		});
+	};
+
+	closeModal = () => {
+		this.setState({
+			modalOpen: false
+		});
+	};
+
+	onSubmit = () => {
+		console.log('tt');
+		this.openModal();
+	}
+
+
 	render() {
+		const {modalOpen, openModal, closeModal} = this.state;
+
 		return (
 			<div id={'wrap'} className={classNames('register_wrap')}>
 				<Aside link={'register'}/>
@@ -50,9 +75,27 @@ class Home extends React.Component {
 					</form>
 
 					<div className={'register_area'}>
-						<button type={'submit'} className={'btn_register'} disabled={'true'}>등록 완료</button>
+						<button type={'submit'} className={'btn_register'} disabled={false} onClick={this.onSubmit} >등록 완료</button>
 					</div>
 				</div>
+
+				<Modal open={modalOpen} close={this.closeModal} header=" ">
+					<div className={'title'}><strong>조영은</strong> 님의 ID와 PW가 생성되었습니다.</div>
+					<p className={'description'}>
+						회원전용 앱을 설치한 후 아래 아이디와 비밀번호를 입력할 수 있습니다. <br/>
+						아이디는 휴대폰 번호이며 비밀번호는 휴대폰 번호 뒷 4자리 입니다.
+					</p>
+					<ul className={'user_information'}>
+						<li className={'row'}>
+							<span className={'cell'}>ID</span>
+							<span className={'cell'}>01012345678</span>
+						</li>
+						<li className={'row'}>
+							<span className={'cell'}>PW</span>
+							<span className={'cell'}>1234</span>
+						</li>
+					</ul>
+				</Modal>
 			</div>
 		);
 	}
